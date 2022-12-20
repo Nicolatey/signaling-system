37603e98eeb5520ba50893531433c060b1298b22
Nicole van der Sluis <nicole.vandersluis@student.fontys.nl>
2022-12-20 12:01:30 +0100

    added imports

--- signaling-system/src/main.cpp
+++ signaling-system/src/main.cpp
@@ -1,7 +1,20 @@
 #include <Arduino.h>
 
+const int buzzer = 3;
+const int redLed = 4; 
+const int greenLed = 5; 
+const int buttonK1 = 8;
+const int buttonK2 = 9;
+// const int microphone = ;
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(buzzer, OUTPUT);
+  pinMode(redLed, OUTPUT);
+  pinMode(greenLed, OUTPUT);
+  pinMode(buttonK1, INPUT);
+  pinMode(buttonK2, INPUT);
+  // pinMode(microphone, );
 }
 
 void loop() {
