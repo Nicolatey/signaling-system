d7ee72c85a84440312cdcd5d66429d8f5b46d6bd
Nicole van der Sluis <nicole.vandersluis@student.fontys.nl>
2022-12-21 21:45:05 +0100

    added potmeter to the board, mapped to de-/increase led brightness

--- signaling-system/src/main.cpp
+++ signaling-system/src/main.cpp
@@ -1,24 +1,32 @@
 #include <Arduino.h>
 
-// const byte potentio = A1;
+const byte potentio = A0;
 const int redLed = 3;
 const int greenLed = 2;
 
+int potValue = 0;
+int ledBrightness = 0;
 
 void setup() {
   Serial.begin(9600);
   pinMode(redLed, OUTPUT);
   pinMode(greenLed, OUTPUT);
-  // pinMode(potentio, INPUT);
+  pinMode(potentio, INPUT);
 }
 
 void loop() {
-  digitalWrite(redLed, HIGH);
-  delay(200);
-  digitalWrite(redLed, LOW);
-  delay(200);
-  digitalWrite(greenLed, HIGH);
-  delay(200);
-  digitalWrite(greenLed, LOW);
-  delay(200);
+  // digitalWrite(redLed, HIGH);
+  // delay(200);
+  // digitalWrite(redLed, LOW);
+  // delay(200);
+  // digitalWrite(greenLed, HIGH);
+  // delay(200);
+  // digitalWrite(greenLed, LOW);
+  // delay(200);
+
+  potValue = analogRead(potentio);
+  Serial.println(potValue);
+
+  ledBrightness = map(potValue, 0, 1023, 0, 255);
+  analogWrite(redLed, ledBrightness);
 }
